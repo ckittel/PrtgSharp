89417fd19d2b682236eacac150d7090f2ed45317
Chad Kittel <chad.kittel@gmail.com>
2017-01-16 12:41:31 -0600

    Add tests for CustomValueUnitProperty

---
+++ PrtgSharp.Tests/ChannelProperties/CustomValueUnitPropertyTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PrtgSharp.ChannelProperties;
+
+namespace PrtgSharp.Tests.ChannelProperties
+{
+    [TestClass]
+    public class CustomValueUnitPropertyTests : ChannelPropertyTestsBase
+    {
+        private static void AssertChannelProp(ChannelProperty channelProp, string value)
+        {
+            AssertChannelProp(channelProp, "customunit", value);
+        }
+
+        [TestMethod]
+        public void UnderMaxLengthValue_Should_Be_CustomUnit_Value()
+        {
+            // Act
+            var channelProp = new CustomValueUnitProperty("Parsecs");
+
+            // Assert
+            AssertChannelProp(channelProp, "Parsecs");
+        }
+
+        [TestMethod]
+        public void AtMaxLengthValue_Should_Be_CustomUnit_Value()
+        {
+            // Act
+            var channelProp = new CustomValueUnitProperty("12345678");
+
+            // Assert
+            AssertChannelProp(channelProp, "12345678");
+        }
+
+        [TestMethod]
+        public void OverMaxLengthValue_Should_Be_CustomUnit_ValueWithElipsis()
+        {
+            // Act
+            var channelProp = new CustomValueUnitProperty("123456789");
+
+            // Assert
+            AssertChannelProp(channelProp, "1234567…");
+        }
+    }
+}
