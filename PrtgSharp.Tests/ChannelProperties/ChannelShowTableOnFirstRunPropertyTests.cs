be8463b20c59a3a7cc2c199cb0a46ae5569a343d
Chad Kittel <chad.kittel@gmail.com>
2017-01-16 11:31:11 -0600

    Add tests for ChannelShowTableOnFirstRunProperty

---
+++ PrtgSharp.Tests/ChannelProperties/ChannelShowTableOnFirstRunPropertyTests.cs
@@ -0,0 +1,34 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PrtgSharp.ChannelProperties;
+
+namespace PrtgSharp.Tests.ChannelProperties
+{
+    [TestClass]
+    public class ChannelShowTableOnFirstRunPropertyTests : ChannelPropertyTestsBase
+    {
+        private static void AssertChannelProp(ChannelProperty channelProp, string value)
+        {
+            AssertChannelProp(channelProp, "showtable", value);
+        }
+
+        [TestMethod]
+        public void Yes_Should_Be_ShowTable_1()
+        {
+            // Act
+            var channelProp = ChannelShowTableOnFirstRunProperty.Yes;
+
+            // Assert
+            AssertChannelProp(channelProp, "1");
+        }
+
+        [TestMethod]
+        public void Yes_Should_Be_ShowChart_0()
+        {
+            // Act
+            var channelProp = ChannelShowTableOnFirstRunProperty.No;
+
+            // Assert
+            AssertChannelProp(channelProp, "0");
+        }
+    }
+}
--- PrtgSharp/ChannelProperties/ChannelShowTableOnFirstRunProperty.cs
+++ PrtgSharp/ChannelProperties/ChannelShowTableOnFirstRunProperty.cs
@@ -3,7 +3,7 @@ namespace PrtgSharp.ChannelProperties
     /// <summary>
     /// Init value for the Show in Table option. Default is 1 (yes). Note: The values defined with this element will be considered only on the first sensor scan, when the channel is newly created; they are ignored on all further sensor scans (and may be omitted). You can change this initial setting later in the Channel settings of the sensor.
     /// </summary>
-    public static class ChannelDefaultShowTableProperty
+    public static class ChannelShowTableOnFirstRunProperty
     {
         public static ChannelProperty No => ShowTable("0");
         public static ChannelProperty Yes => ShowTable("1");
