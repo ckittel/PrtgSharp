e8d38add6d018e245b77c077c52d33334a45b739
Chad Kittel <chad.kittel@gmail.com>
2017-01-16 15:38:11 -0600

    Add LimitMode Add Unit Tests

---
+++ PrtgSharp.Tests/ChannelProperties/LimitModeOnFirstRunPropertyTests.cs
@@ -0,0 +1,34 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PrtgSharp.ChannelProperties;
+
+namespace PrtgSharp.Tests.ChannelProperties
+{
+    [TestClass]
+    public class LimitModeOnFirstRunPropertyTests : ChannelPropertyTestsBase
+    {
+        private static void AssertChannelProp(ChannelProperty channelProp, string value)
+        {
+            AssertChannelProp(channelProp, "limitmode", value);
+        }
+
+        [TestMethod]
+        public void Active_Should_Be_LimitMode_1()
+        {
+            // Act
+            var channelProp = LimitModeOnFirstRunProperty.Active;
+
+            // Assert
+            AssertChannelProp(channelProp, "1");
+        }
+
+        [TestMethod]
+        public void Inactive_Should_Be_LimitMode_0()
+        {
+            // Act
+            var channelProp = LimitModeOnFirstRunProperty.Inactive;
+
+            // Assert
+            AssertChannelProp(channelProp, "0");
+        }
+    }
+}
